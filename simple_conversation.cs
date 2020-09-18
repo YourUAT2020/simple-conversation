ccd77e150e6995342d99e6c3b31964fb71161bd8
YourUAT2020 <71243654+YourUAT2020@users.noreply.github.com>
2020-09-17 22:07:50 -0700

    Add simple conversation

---
+++ simple_conversation.cs
@@ -0,0 +1,12 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+         System.Console.WriteLine("hello");
+         System.Console.WriteLine("Vertis M. Wilder");
+         System.Console.WriteLine("Did you sleep well yesterday");
+         System.Console.WriteLine("When you get time give me a call bye.");
+         System.Console.WriteLine("Let your teacher know that you are upgrading your computer.");
+         System.Console.WriteLine("I will make sure that I get my assignments in on time after this. I have been juggling 2 jobs and working towards changing my license to Arizona license so I can vote.");
+  }
+}
